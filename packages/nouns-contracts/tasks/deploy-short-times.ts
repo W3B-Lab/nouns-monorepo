07f36f56d0262bc57c0d335ac8c57f484545e386
Martin Lippert <martin@enlighten-media.net>
2022-10-05 13:06:43 +0200

    Goerli updates

--- packages/nouns-contracts/tasks/deploy-short-times.ts
+++ packages/nouns-contracts/tasks/deploy-short-times.ts
@@ -16,7 +16,8 @@ const wethContracts: Record<number, string> = {
   [ChainId.Mainnet]: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
   [ChainId.Ropsten]: '0xc778417e063141139fce010982780140aa0cd5ab',
   [ChainId.Rinkeby]: '0xc778417e063141139fce010982780140aa0cd5ab',
-  [ChainId.Kovan]: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
+  [ChainId.Kovan]: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',,
+  [ChainId.Goerli]: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
 };
 
 const NOUNS_ART_NONCE_OFFSET = 4;
